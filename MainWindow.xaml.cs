06b83c7b70c086d581d46cb53c2e220d793de303
Igor Cherkesov <i.4erkesov@gmail.com>
2020-10-19 09:44:44 +0300

    Finaly

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using AdressBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace AdressBook
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+        
+    {
+        UserContext db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new UserContext();
+            db.Users.Load(); // загружаем данные
+            UsersGrid.ItemsSource = db.Users.Local.ToBindingList(); // устанавливаем привязку к кэшу
+
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Dispose();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            db.SaveChanges();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UsersGrid.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < UsersGrid.SelectedItems.Count; i++)
+                {
+                    User user = UsersGrid.SelectedItems[i] as User;
+                    if (user != null)
+                    {
+                        db.Users.Remove(user);
+                    }
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook.Models
+{
+    class UserContext : DbContext
+    {
+
+        public UserContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
